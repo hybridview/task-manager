002f97f1b43f02fab39cf1401d401666a338bab1
Andrew M <andrewbmoore@gmail.com>
2023-02-28 12:25:00 -0500

    Several more dependency updates and move to React 18.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,14 +2,17 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 
 import App from 'App';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root'),
 );
